bc5b6beff9740fdab2f8cbbe6a738a7d9aa4de52
udiptaweb <gogoiudipta000@gmail.com>
2023-10-11 21:52:28 +0530

    middleware added to route

--- routes/admin.php
+++ routes/admin.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Admin\RolePermissionController;
 use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/admin/role-permissions',[RolePermissionController::class,'index'])->name('admin.role-permissions');
+Route::get('/admin/role-permissions',[RolePermissionController::class,'index'])->middleware(['role:Super Admin'])->name('admin.role-permissions');
 Route::get('/admin/users',[UserController::class,'index'])->name('admin.users.index');
 Route::get('/admin/create-user',[UserController::class,'create'])->name('admin.users.create');
 Route::get('/admin/edit-user/{user_id}',[UserController::class,'edit'])->name('admin.users.edit');
